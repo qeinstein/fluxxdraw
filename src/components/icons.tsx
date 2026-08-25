e4fdee98a02b4359fb99be7d8475f95263ffbca3
qeinstein <ogunadetoheeb4@gmail.com>
2026-08-25 13:50:14 +0100

    feat: integrate excalidraw community libraries via neon database
    Signed-off-by: qeinstein <ogunadetoheeb4@gmail.com>

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -406,6 +406,12 @@ export const IconCheck = () => (
   </Icon>
 );
 
+export const IconLibrary = () => (
+  <Icon>
+    <path d="M4 19.5v-15A2.5 2.5 0 0 1 6.5 2H20v20H6.5a2.5 2.5 0 0 1 0-5H20"/>
+  </Icon>
+);
+
 export const IconMore = () => (
   <Icon>
     <circle cx="4.6" cy="10" r="1.3" fill="currentColor" stroke="none" />
